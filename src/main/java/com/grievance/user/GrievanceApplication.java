202065dd8d5b4628d0ef8685f1dfb1ee35734d9d
ashwani8418 <68030476+ashwani8418@users.noreply.github.com>
2023-10-24 23:46:48 +0530

    First Commit

---
+++ src/main/java/com/grievance/user/GrievanceApplication.java
@@ -0,0 +1,13 @@
+package com.grievance.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrievanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrievanceApplication.class, args);
+    }
+
+}
